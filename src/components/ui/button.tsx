7f60645a695fe5ec4eeef5d472618b55d6e700bb
LazY <darshitmistry2@gmail.com>
2024-04-29 08:31:16 +0530

    feat : file upload

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,6 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        normal : "bg-[#20413e] text-white hover:bg-[#20413e]"
       },
       size: {
         default: "h-10 px-4 py-2",
